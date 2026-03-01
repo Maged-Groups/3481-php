024e625e1d380d04251f62ecf03e7dd93f91f30a
Aly Mohamed <aly.mohamed2707@gmail.com>
2026-03-01 13:10:50 +0200

    Question 3

--- ALY/Chall_14/index.php
+++ ALY/Chall_14/index.php
@@ -17,5 +17,18 @@ $square = array_map(function ($number) {
     $numbers
 );
 
+## Question 3
+
+$users = [
+    ['name' => 'Ahmed', 'age' => 22],
+    ['name' => 'Sara', 'age' => 35],
+    ['name' => 'Mona', 'age' => 28],
+    ['name' => 'Ali', 'age' => 40],
+];
+
+$age = array_filter($users, function ($user) {
+    return $user['age'] > 30;
+});
+
 
 
