d2bf17b4e329bd800d75fe543492dca8c08da71f
Aly Mohamed <aly.mohamed2707@gmail.com>
2026-04-04 14:22:10 +0200

    Request Method

--- Aly/users/store.php
+++ Aly/users/store.php
@@ -1,5 +1,23 @@
 <?php
 
-var_dump($GLOBALS);
+// var_dump($GLOBALS['_SERVER']);
+// var_dump($_SERVER);
+var_dump($_SERVER['REQUEST_METHOD']);
+// var_dump($_FILES['photo']);
+// var_dump($_POST);
+
+$method = $_SERVER['REQUEST_METHOD'] ?? '';
+
+if($method !== 'POST') die('The request method is not allowed, only POST method is allowed!!!');
+
+
+
+
+
+$languages = $_POST['lang'] ?? [];
+var_dump($languages);
+
+
+
 
 echo "Store new user";
